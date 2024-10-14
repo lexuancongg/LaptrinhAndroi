343254e3c697d8f38ccf32ba36998dff959acc44
Minh Huy 1003 <hophanminhhuy10032004@gmail.com>
2024-10-14 21:09:27 +0700

    minhhuy

--- app/src/main/java/com/example/bai1/input_fragment/TienThuFragment.java
+++ app/src/main/java/com/example/bai1/input_fragment/TienThuFragment.java
@@ -1,5 +1,6 @@
 package com.example.bai1.input_fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import com.example.bai1.Fragment1.GridViewAdapter;
 import com.example.bai1.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TienThuFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TienThuFragment extends Fragment {
+    String[] ten_thu = {
+            "Xe cộ", "Tiền nhà", "Dự phòng", "Tiền ăn",
+            "Xe cộ", "Tiền nhà", "Dự phòng", "Tiền ăn","Xe cộ", "Tiền nhà", "Dự phòng", "Tiền ăn",
+            "Xe cộ", "Tiền nhà", "Dự phòng", "Tiền ăn",
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    };
+    GridView gridView;
+    int[] hinh_thu = {
+            R.drawable.car, R.drawable.home, R.drawable.money, R.drawable.restaurant,
+            R.drawable.car, R.drawable.home, R.drawable.money, R.drawable.restaurant,
+            R.drawable.car, R.drawable.home, R.drawable.money, R.drawable.restaurant,
+            R.drawable.car, R.drawable.home, R.drawable.money, R.drawable.restaurant,
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    };
 
-    public TienThuFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TienThuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TienThuFragment newInstance(String param1, String param2) {
-        TienThuFragment fragment = new TienThuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tien_thu, container, false);
+        View view = inflater.inflate(R.layout.fragment_tien_thu, container, false);
+
+        gridView = view.findViewById(R.id.gridView_thu); // Sử dụng view đã inflate
+        GridViewAdapter adapter = new GridViewAdapter(requireContext(), ten_thu, hinh_thu);
+        gridView.setAdapter(adapter);
+
+        return view;
     }
 }
