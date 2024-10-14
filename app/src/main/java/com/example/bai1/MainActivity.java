7a593afe42fd9443e6cc64bdd425614539393993
Nguyen Thi Thu <hientran02091979@gmail.com>
2024-10-15 01:21:37 +0700

    thuthu

--- app/src/main/java/com/example/bai1/MainActivity.java
+++ app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,66 +1,61 @@
 package com.example.bai1;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback;
 
 import com.example.bai1.Fragment1.ViewPagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager;
     private BottomNavigationView bottomNavigationView;
 
-
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        EdgeToEdge.enable(this);
+        // Cần gọi setContentView trước khi sử dụng findViewById
         setContentView(R.layout.activity_main);
 
-        //set Fragment1
+
+        // Thiết lập ViewPager và BottomNavigationView
         viewPager = findViewById(R.id.view_pager);
-        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set ViewPager Adapter
         ViewPagerAdapter adapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(adapter);
 
-        //gridView
+        // Lắng nghe sự kiện thay đổi trang
+        viewPager.registerOnPageChangeCallback(new OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_input).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_calendar).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_add).setChecked(true);
+                        break;
+                }
+            }
+        });
 
-
-       viewPager.registerOnPageChangeCallback(new OnPageChangeCallback() {
-           @Override
-           public void onPageSelected(int position) {
-               switch (position){
-                   case 0:
-                       bottomNavigationView.getMenu().findItem(R.id.menu_input).setChecked(true);
-                       break;
-                   case 1:
-                       bottomNavigationView.getMenu().findItem(R.id.menu_calendar).setChecked(true);
-                       break;
-                   case 2:
-                       bottomNavigationView.getMenu().findItem(R.id.menu_add).setChecked(true);
-                       break;
-               }
-           }
-       });
+        // Lắng nghe sự kiện chọn item trong BottomNavigationView
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -75,8 +70,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
     }
-
 }
